f91fd2f0c8bbf373cc0ea7460ac3d0f055334347
bintangpr <bintangpr175@gmail.com>
2023-01-18 13:42:51 +0700

    initialize

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      screens: {
+      xxs: { min: '0px', max: '320px'},
+      xs: { min: '321px' },
+      sm: { min: '640px' },
+      md: { min: '768px' },
+      lg: { min: '1024px' },
+      xl: { min: '1280px' },
+    },
+      fontFamily: {
+      inter: "'Inter', serif",
+      poppins: "'Poppins', serif"   
+    },
+    },
+  },
+  plugins: [],
+}
