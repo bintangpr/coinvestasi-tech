ed0382d873282ddc8df5981feae7088bad38f8a3
bintangpr <bintangpr175@gmail.com>
2023-01-22 08:56:47 +0700

    fix box bug

--- src/component/pageInformation.js
+++ src/component/pageInformation.js
@@ -5,8 +5,8 @@ import timer from '../aset/timer.svg'
 const PageInformation = (props) => {
     const showPage = props.info.map((item, index) => {
         return <div key={index} className={ index === props.info.length -1 ? "text-greyText flex w-32 xs:w-40 sm:w-auto" : "text-bluePage flex"} >
-            <div className='w-6 h-6'>
-            {index === 0 ? "" : <img src={ArrowRight} className='w-6 h-6 sm:mr-2' alt='right arrow' />}
+            <div className={index === 0 ? "" :'w-6 h-6'}>
+                {index === 0 ? "" : <img src={ArrowRight} className='w-6 h-6 sm:mr-2' alt='right arrow' />}
             </div> 
             <p className={index === props.info.length -1 ? "truncate text-ellipsis w-1/2 sm:w-auto" : ""}>{item}</p> 
             </div>;
